07981d13b88a5aa897e12861882a47e185a97df5
r3garciaz <garcia.reinaldo@gmail.com>
2025-07-25 15:00:11 -0400

    Add entity configurations and migration for Congregation, Department, MeetingSchedule, MeetingType, Publisher, Responsibility, ResponsibilityAssignment, and ResponsibilityAssignmentConfig

--- GestorTeocratico/Data/ApplicationDbContext.cs
+++ GestorTeocratico/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using GestorTeocratico.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,4 +7,145 @@ namespace GestorTeocratico.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+    
+    public DbSet<Congregation> Congregations { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<MeetingSchedule> MeetingSchedules { get; set; }
+    public DbSet<MeetingType> MeetingTypes { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<PublisherResponsibility> PublisherResponsibilities { get; set; }
+    public DbSet<Responsibility> Responsibilities { get; set; }
+    public DbSet<ResponsibilityAssignment> ResponsibilityAssignments { get; set; }
+    public DbSet<ResponsibilityAssignmentConfig> ResponsibilityAssignmentConfigs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Congregation>(entity =>
+        {
+            entity.Property(p => p.Name).HasMaxLength(250).IsRequired();
+            entity.Property(c => c.MidweekMeetingDayEvenYear).IsRequired().HasConversion<string>();
+            entity.Property(c => c.MidweekMeetingDayOddYear).IsRequired().HasConversion<string>();
+            entity.Property(c => c.WeekendMeetingDayEvenYear).IsRequired().HasConversion<string>();
+            entity.Property(c => c.WeekendMeetingDayOddYear).IsRequired().HasConversion<string>();
+            entity.Property(p => p.Address).HasMaxLength(500);
+            entity.Property(p => p.City).HasMaxLength(250);
+
+            entity.HasMany(c => c.Departments)
+                .WithOne(d => d.Congregation)
+                .HasForeignKey(d => d.CongregationId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasMany(c => c.Publishers)
+                .WithOne(p => p.Congregation)
+                .HasForeignKey(p => p.CongregationId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            entity.HasMany(c => c.MeetingSchedules)
+                .WithOne(ms => ms.Congregation)
+                .HasForeignKey(ms => ms.CongregationId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+        
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.Property(p => p.Name).HasMaxLength(250).IsRequired();
+
+            entity.HasMany(d => d.Responsibilities)
+                .WithOne(r => r.Department)
+                .HasForeignKey(r => r.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+        
+        modelBuilder.Entity<MeetingSchedule>(entity =>
+        {
+            entity.Property(ms => ms.Date).IsRequired();
+            entity.Property(ms => ms.Month).IsRequired();
+            entity.Property(ms => ms.Year).IsRequired();
+            entity.Property(ms => ms.WeekOfYear).IsRequired();
+            entity.Property(ms => ms.MeetingTypeId).IsRequired();
+            
+            entity.HasMany(m => m.ResponsibilityAssignments)
+                .WithOne(ra => ra.MeetingSchedule)
+                .HasForeignKey(ra => ra.MeetingScheduleId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<MeetingType>(entity =>
+        {
+            entity.Property(mt => mt.Name).HasMaxLength(250).IsRequired();
+            entity.Property(mt => mt.Description).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<Publisher>(entity =>
+        {
+            entity.Property(p => p.FirstName).HasMaxLength(250).IsRequired();
+            entity.Property(p => p.LastName).HasMaxLength(250);
+            entity.Property(p => p.MotherLastName).HasMaxLength(250);
+            entity.Property(p => p.Phone).HasMaxLength(15);
+            entity.Property(p => p.Email).HasMaxLength(250);
+            entity.Property(p => p.Gender).IsRequired();
+            entity.Property(p => p.Privilege).IsRequired();
+            
+            entity.HasOne(p => p.Congregation)
+                .WithMany(c => c.Publishers)
+                .HasForeignKey(p => p.CongregationId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            entity.HasMany(p => p.PublisherResponsibilities)
+                .WithOne(pr => pr.Publisher)
+                .HasForeignKey(pr => pr.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<PublisherResponsibility>(entity =>
+        {
+            entity.HasKey(pr => new { pr.PublisherId, pr.ResponsibilityId });
+            entity.HasOne(pr => pr.Publisher)
+                .WithMany(p => p.PublisherResponsibilities)
+                .HasForeignKey(pr => pr.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+            entity.HasOne(pr => pr.Responsibility)
+                .WithMany(r => r.PublisherResponsibilities)
+                .HasForeignKey(pr => pr.ResponsibilityId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Responsibility>(entity =>
+        {
+            entity.Property(r => r.Name).HasMaxLength(100).IsRequired();
+            entity.Property(r => r.Description).HasMaxLength(500);
+            entity.Property(r => r.IsActive).IsRequired();
+            entity.HasMany(r => r.PublisherResponsibilities)
+                .WithOne(pr => pr.Responsibility)
+                .HasForeignKey(pr => pr.ResponsibilityId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<ResponsibilityAssignment>(entity =>
+        {
+            entity.HasKey(ra => new { ra.MeetingScheduleId, ra.ResponsibilityId, ra.PublisherId });
+            
+            entity.HasOne(ra => ra.MeetingSchedule)
+                .WithMany(ms => ms.ResponsibilityAssignments)
+                .HasForeignKey(ra => ra.MeetingScheduleId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            entity.HasOne(ra => ra.Publisher)
+                .WithMany()
+                .HasForeignKey(ra => ra.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            entity.HasOne(ra => ra.Responsibility)
+                .WithMany()
+                .HasForeignKey(ra => ra.ResponsibilityId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<ResponsibilityAssignmentConfig>(entity =>
+        {
+            entity.HasKey(rac => new { rac.ResponsibilityId, rac.MeetingTypeId });
+            entity.Property(rac => rac.Quantity).IsRequired().HasDefaultValue(1);
+        });
+    }
 }
