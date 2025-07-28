94fcb206ffea2fe93f7c6253e2216920a1d53068
r3garciaz <garcia.reinaldo@gmail.com>
2025-07-28 17:37:40 -0400

    Refactor services to remove unnecessary dependencies and replace service-based lookups with direct DbContext calls, improving efficiency and reducing complexity.

--- GestorTeocratico/Features/Responsibilities/IResponsibilityService.cs
+++ GestorTeocratico/Features/Responsibilities/IResponsibilityService.cs
@@ -4,11 +4,12 @@ namespace GestorTeocratico.Features.Responsibilities;
 
 public interface IResponsibilityService
 {
-    Task<IEnumerable<Responsibility>> GetAllAsync();
+    Task<IQueryable<Responsibility>> GetAllAsync();
     Task<Responsibility?> GetByIdAsync(Guid id);
-    Task<Responsibility> CreateAsync(Responsibility responsibility);
-    Task<Responsibility?> UpdateAsync(Responsibility responsibility);
-    Task<bool> DeleteAsync(Guid id);
-    Task<IEnumerable<Responsibility>> GetByDepartmentIdAsync(Guid departmentId);
-    Task<IEnumerable<Responsibility>> GetAvailableForAssignmentAsync();
+    Task AddAsync(Responsibility responsibility);
+    Task UpdateAsync(Responsibility responsibility);
+    Task<Responsibility> DeleteAsync(Guid id);
+    Task<IEnumerable<Department>> GetAvailableDepartmentsAsync();
+    Task<IEnumerable<Publisher>> GetAvailablePublishersAsync();
+    Task UpdateResponsibilityPublishersAsync(Guid responsibilityId, IEnumerable<Guid> publisherIds);
 }
