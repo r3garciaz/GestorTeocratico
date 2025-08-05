289422f7c285d8f9b2050ab1c2a442458ef117ad
r3garciaz <garcia.reinaldo@gmail.com>
2025-08-05 19:52:54 -0400

    Make LastName property required in Publisher entity and update related migration

---
+++ GestorTeocratico/Data/Migrations/20250805235204_publisher_modify_properties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestorTeocratico.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class publisher_modify_properties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Publishers",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Publishers",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
