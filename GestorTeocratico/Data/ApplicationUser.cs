85593c75334dc9f5e0a878cccb8e7788ebda5f51
r3garciaz <garcia.reinaldo@gmail.com>
2025-08-19 14:08:29 -0400

    Add PublicId property to ApplicationUser: update ApplicationUser class, modify ApplicationDbContext for unique indexing, and create migration for database schema changes.

--- GestorTeocratico/Data/ApplicationUser.cs
+++ GestorTeocratico/Data/ApplicationUser.cs
@@ -5,4 +5,5 @@ namespace GestorTeocratico.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string PublicId { get; set; } = string.Empty;    
 }
